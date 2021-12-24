8cd17ddf4b4e956a9e98f51c4be5de4f256e114d
Aaron-MCLNZ <aaron.buchan@mclarens.co.nz>
2021-12-24 15:24:57 +1300

    add keyvault

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the Azure provider
+
 terraform {
   required_providers {
     azurerm = {
@@ -10,13 +10,25 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
+# Configure the Azure provider
 provider "azurerm" {
-  features {}
+  features {
+      key_vault {
+      purge_soft_delete_on_destroy = true
+      }
+
+  subscription_id = var.subscription_id 
+  client_id = var.client_id
+  client_certificate_path = var.client_certificate_path
+  client_certificate_password = var.client_certificate_password
+  tenant_id = var.tenant_id
+  }
+  
 }
 
 # Create Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "moderg" {
+  name     = var.moderg
   location = var.location
 
   tags = {
@@ -25,13 +37,44 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+# Create Key Vault
+
+resource "azurerm_key_vault" "modekv" {
+  name                        = var.modekv
+  location                    = var.location
+  resource_group_name         = var.moderg
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
 # Create Storage Account
 module "storage_account" {
   source    = "./modules/storage-account"
 
   saname    = "ModeStorage"
-  rgname    = azurerm_resource_group.rg.name
-  location  = azurerm_resource_group.rg.location
+  rgname    = azurerm_resource_group.moderg.name
+  location  = azurerm_resource_group.moderg.location
 }
 
 # Save Storage Key to Vault
@@ -48,13 +91,13 @@ resource "azurerm_virtual_network" "vnet" {
   name     = "ModeVnet"
   address_space = ["10.0.0.0/16"]
   location = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.moderg.name
 }
 
 # Create Azure Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "mode-prod-auseast-001 "
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.moderg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.3.0.0/24"]
 }
@@ -63,7 +106,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_public_ip" "publicip" {
   name                = "pip-mode-prod-auseast-001"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.moderg.name
   allocation_method   = "Static"
 
   tags = {
@@ -76,7 +119,7 @@ resource "azurerm_public_ip" "publicip" {
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-sshallow-001 "
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.moderg.name
 
   security_rule {
     name                       = "SSH"
@@ -95,7 +138,7 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_network_interface" "nic" {
   name                      = "nic-01-modevm-prod-001 "
   location                  = var.location
-  resource_group_name       = azurerm_resource_group.rg.name
+  resource_group_name       = azurerm_resource_group.moderg.name
   #network_security_group_id = azurerm_network_security_group.nsg.name
 
   ip_configuration {
@@ -110,7 +153,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_virtual_machine" "vm" {
   name                  = "modevm01"
   location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = azurerm_resource_group.moderg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_B1s"
 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
-variable "resource_group_name" {
+variable "moderg" {
+  type = string
+  description = "Mode Resources Group"
   default = "ModeResourceGroup"
 }
 
@@ -6,4 +8,40 @@ variable "location" {
     type = string
     description = "Azure location of storage account environment"
     default = "AustraliaSoutheast"
+}
+
+variable "client_certificate_path" {
+    type = string
+    description = "Path to Client Certificate"
+    default = "value"
+}
+
+variable "client_certificate_password" {
+    type = string
+    description = "Password for Client Certificate"
+    default = "value"
+}
+
+variable "subscription_id" {
+  type = string
+  description = "Subscription ID of Tenant"
+  default = ""
+}
+
+variable "client_id" {
+  type = string
+  description = "ID of client connection"
+  default = ""
+}
+
+variable "tenant_id" {
+  type = string
+  description = "ID of Tenant"
+  default = ""
+}
+
+variable "modekv" {
+  type = string
+  description = "Name of Keyvault"
+  default = "modekeyvault"
 }
